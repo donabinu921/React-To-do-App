641e098fd07813e094952598851f392233fe1743
donabinu921 <elizabethdona921@gmail.com>
2024-02-19 14:59:30 +0530

    commit 2

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export const TodoForm = () => {
+export const TodoForm = ({addTodo}) => {
+
+  const [value,setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo(value)
+    setValue("")
+  }
   return (
-    <form className='TodoForm'>
-      <input className="todo-input" type="text" placeholder='What is the task today?' />
-      <button className='todo-btn' type='submit'>Add</button>
+    <form className='TodoForm' onSubmit={handleSubmit}>
+      <input 
+        className="todo-input" 
+        type="text" 
+        placeholder='What is the task today?'
+        onChange={(e)=>setValue(e.target.value)}
+        value={value}
+      />
+      <button 
+        className='todo-btn' 
+        type='submit'>
+        Add Task
+      </button>
     </form>
   )
 }
